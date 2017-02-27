03a942290b0d5e652d2aa480325582348785ea4a
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-27 17:13:32 +0300

    JsonResult Nedir ?

---
+++ 12_ActionTurleri/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using _12_ActionTurleri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _12_ActionTurleri.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        #region Redirect Result Sample
+        public ActionResult Index()
+        {
+            return Redirect("https://www.google.com");
+        }
+
+        public RedirectResult Index2()
+        {
+
+            return Redirect("/Home/Index");
+        }
+        public RedirectToRouteResult Index3()
+        {
+            return RedirectToAction("Index2");
+        }
+        #endregion
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+
+        public JsonResult Index5()
+        {
+            Urun urn = new Urun();
+            urn.Adi = "Domates";
+            urn.Fiyat = 12000;
+            urn.ID = 5;
+            urn.Aciklama = "Bu bir deneme ürünüdür.";
+
+
+            /* Json Böyle;
+             {
+                ID=5,
+                Adi:"Domates",
+                Fiyat:12000,
+                Aciklama:"Bu bir deneme ürünüdür."
+             }
+
+
+            XML Böyle;
+            <Urun>
+                   <ID>5</ID>
+                   <Adi>Domates</Adi>
+                   <Fiyat>12000</Fiyat>
+                   <Aciklama>Bu bir deneme ürünüdür.<Aciklama>
+
+            </Urun>
+             
+             */
+
+            return Json(urn,JsonRequestBehavior.AllowGet); // Json > verdigimiz urn nesnesini Json turune donusturur.
+        }
+        [HttpPost]   //Bu post metodunu ajax kisminda  type: "POST", post ile veri cekmek isteyeni engellemek icin yazdik.
+        public JsonResult Index5(int? deger)
+        {
+            return Json(null);
+        }
+    }
+}
---
+++ 12_ActionTurleri/Models/Urun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _12_ActionTurleri.Models
+{
+    public class Urun
+    {
+        public int ID { get; set; }
+        public string Adi { get; set; }
+        public int Fiyat { get; set; }
+        public string Aciklama { get; set; }
+
+    }
+}
